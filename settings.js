ad447cf7ac1a5c325b3f849b054c9ab091f952b6
Lightoftheworld2 <emperorray969@gmail.com>
2024-10-08 09:53:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2347041522894"
+global.ownername = "light of the world"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2347041522894'  //creator number
+global.ownername = 'light of the world' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
